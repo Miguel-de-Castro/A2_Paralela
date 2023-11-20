97a028be473e3a97cc1ae3760b4d36c246c2a349
miguel-de-castro <miguel.c@edu.pucrs.br>
2023-11-20 10:46:33 -0300

    print math

--- sequencial.c
+++ sequencial.c
@@ -182,6 +182,7 @@ int main(int argc, char *argv[])
                 mres[i][j] = 0;
                 for (k = 0; k < SIZE; k++)
                 {
+                    printf("i: %d, j: %d, k: %d, m1[i][k]: %d, m2[k][j]: %d\n", i, j, k, m1[i][k], m2[k][j]);
                     mres[i][j] += m1[i][k] * m2[k][j];
                 }
             }
