7acd27b26192ae63c7737166f1d7bacca6b6463a
miguel-de-castro <miguel.c@edu.pucrs.br>
2023-11-20 00:34:25 -0300

    commit 1

--- sequencial.c
+++ sequencial.c
@@ -147,7 +147,10 @@ int main(int argc, char *argv[])
         printf("id: %d, offset: %d\n", id, offset);
         MPI_Recv(&chunkSize, 1, MPI_INT, MESTREID, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&m1[offset][0], chunkSize * SIZE, MPI_INT, MESTREID, 0, MPI_COMM_WORLD, &status);
-         printf("de: %d, para: %d\n", offset, chunkSize + offset);
+        printf("de: %d, para: %d\n", offset, chunkSize + offset);
+
+        omp_set_num_threads(15);
+
 #pragma omp parallel for
         for (i = offset; i < chunkSize + offset; i++)
         {
