94c1e09158d88c2615faaf722b0300360cd4a38d
miguel-de-castro <miguel.c@edu.pucrs.br>
2023-11-22 11:29:38 -0300

    for update

--- run_jobs.sh
+++ run_jobs.sh
@@ -4,7 +4,7 @@
 batchjob="batchjob_parameter.batchjob"
 
 # Loop para variar o número de nodos de 1 a 4
-for nodes in {1..4..1}; do
+for nodes in $(seq 1 4); do
   # Submetendo um trabalho SLURM para execução
   sbatch --nodes $nodes $batchjob sequencial $nodes
 done
