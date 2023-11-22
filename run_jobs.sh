1eb5c8957292b72819db9a25b90392e954e8140d
miguel-de-castro <miguel.c@edu.pucrs.br>
2023-11-22 12:22:20 -0300

    teste

--- run_jobs.sh
+++ run_jobs.sh
@@ -4,18 +4,21 @@
 batchjob="batchjob_parameter.batchjob"
 program=sequencial
 
-job_ids=()
-
 for nodes in $(seq 1 4); do
   # Submetendo um trabalho SLURM para execução e obtendo o ID do trabalho
   job_id=$(sbatch --nodes $nodes $batchjob $program $nodes | awk '{print $4}')
-  job_ids+=($job_id)
-done
-
-# Esperar pela conclusão de todos os trabalhos SLURM
-for job_id in "${job_ids[@]}"; do
   # Utilizar squeue para verificar a conclusão do trabalho
+  while squeue --noheader -j $job_id &>/dev/null; do
+    sleep 1
+  done
     more batchjob_parameter.batchjob.$job_id.out
     echo ""
-    echo ""
 done
+
+# # Esperar pela conclusão de todos os trabalhos SLURM
+# for job_id in "${job_ids[@]}"; do
+#   # Utilizar squeue para verificar a conclusão do trabalho
+#     more batchjob_parameter.batchjob.$job_id.out
+#     echo ""
+#     echo ""
+# done
