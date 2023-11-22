e7b118d50ab8863dd4e64f6a00713c2d8ee89489
miguel-de-castro <miguel.c@edu.pucrs.br>
2023-11-22 11:25:04 -0300

    teste auto

---
+++ run_jobs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Nome do seu arquivo batchjob
+batchjob="batchjob_parameter.batchjob"
+
+# Loop para variar o número de nodos de 1 a 4
+for nodes in {1..4}; do
+  # Submetendo um trabalho SLURM para execução
+  sbatch --nodes $nodes $batchjob sequencial $nodes
+done
